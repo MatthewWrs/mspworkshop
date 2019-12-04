bd063f32d236715b8128d365ed92b70fe07d4586
Matthew <matthew.wrs@gmail.com>
2019-12-03 22:27:57 -0500

    Title change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Table Tennis' });
+  res.render('index', { title: 'Only people who are cool play pingpong' });
 });
 
 module.exports = router;
